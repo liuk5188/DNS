ac8fe2350adff1955a3d9b1a62ec389531b856ce
Jason He <59273498+JasonHe@users.noreply.github.com>
2021-05-18 11:36:25 +0800

    Update dns_us.sh

--- dns_us.sh
+++ dns_us.sh
@@ -4,4 +4,5 @@ touch /etc/resolv.conf
 echo -e "nameserver 129.159.45.97\nnameserver 8.8.8.8" > /etc/resolv.conf
 echo "Netflix解锁（美国）已设置完成。本机IP："
 curl ifconfig.me
+echo ""
 echo "请将本机IP地址反馈给我添加白名单即可完成解锁。"
